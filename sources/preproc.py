3f0799430ff9f2503d1f0d0feda648cdfa07ca0e
Luis Pedro Coelho <luis@luispedro.org>
2012-08-06 04:13:49 +0100

    RFCT Make preprocessing a function

--- sources/preproc.py
+++ sources/preproc.py
@@ -1,16 +1,18 @@
 from load import *
-gene_exp,celltypes_ge,_ = read_gene_expression()
-training,celltypes,cs = read_training()
-labels = []
-features = []
-for ci,ct in enumerate(celltypes):
-    try:
-        id = celltypes_ge.index(ct)
-        features.append(gene_exp[:,id])
-        labels.append(training[ci])
-    except ValueError:
-        pass
-    
-features = np.array(features)
-labels = np.array(labels)
-labels[np.isnan(labels)] = 1+np.nanmax(labels)
+def preproc():
+    gene_exp,celltypes_ge,_ = read_gene_expression()
+    training,celltypes,cs = read_training()
+    labels = []
+    features = []
+    for ci,ct in enumerate(celltypes):
+        try:
+            id = celltypes_ge.index(ct)
+            features.append(gene_exp[:,id])
+            labels.append(training[ci])
+        except ValueError:
+            pass
+        
+    features = np.array(features)
+    labels = np.array(labels)
+    labels[np.isnan(labels)] = 1+np.nanmax(labels)
+    return features, labels
