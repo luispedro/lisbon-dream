95dce63eb7c421687c8218ce45ae751c21634650
Luis Pedro Coelho <luis@luispedro.org>
2012-08-11 03:49:38 +0100

    ENH Do not consider NaN values

--- sources/projection.py
+++ sources/projection.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 class product_predictor(object):
     def __init__(self, x):
         self.x = x
@@ -9,14 +10,19 @@ class product_predictor(object):
 class random_project(object):
     def __init__(self, nr_dims=12):
         self.nr_dims = nr_dims
+
     def train(self, features, labels):
-        n,m = features.shape
-        V = np.random.rand(m, self.nr_dims)
+        nr_celltypes,nr_features = features.shape
+        nr_celltypes_prime,nr_drugs = labels.shape
+        assert nr_celltypes_prime == nr_celltypes
+        V = np.random.rand(nr_features, self.nr_dims)
         features = np.dot(features, V)
         features /= features.mean()
         xs = []
-        for i in xrange(n):
-            x,residues,rank,s = np.linalg.lstsq(features, labels[:,i])
+        for ci in xrange(nr_drugs):
+            clabels = labels[:,ci]
+            active = ~np.isnan(clabels)
+            x,residues,rank,s = np.linalg.lstsq(features[active], labels[active,ci])
             xs.append(x)
         xs = np.array(xs)
         return product_predictor(np.dot(V,xs.T))
