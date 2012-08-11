dc60e9ec23663fd6748bfd88412db90612716b2f
Luis Pedro Coelho <luis@luispedro.org>
2012-08-11 03:00:07 +0100

    ENH Correct result evaluation

--- sources/leave1out.py
+++ sources/leave1out.py
@@ -1,15 +1,27 @@
 from scipy import stats
 import numpy as np
 
+def compare(predicted, gold):
+    active = ~np.isnan(gold)
+    return stats.spearmanr(predicted[active], gold[active])
+
 def leave1out(learner, features, labels):
     '''
     avg_corr = leave1out(learner, features, labels):
     '''
-    corrs = []
+    predicted = []
     for i in xrange(len(labels)):
         idx = np.ones(len(labels), bool)
         idx[i] = 0
-        p = learner.train(features[idx], labels[idx])
-        corr,ps = stats.spearmanr(p.apply_one(features[i]), labels[i])
+        model = learner.train(features[idx], labels[idx])
+        predicted.append(model.apply_one(features[i]))
+    predicted = np.array(predicted)
+    predicted = predicted.T
+
+    corrs = []
+    pvals = []
+    for p in predicted:
+        corr,ps = compare(p, labels[i])
         corrs.append(corr)
+        pvals.append(ps)
     return np.mean(corrs)
