d3c2ffd6d8cab71fa62e2205c4cbaece3ae7f6d4
Luis Pedro Coelho <luis@luispedro.org>
2012-09-06 15:02:28 +0100

    ENH Juggify lisbon-dream

--- sources/jugfile.py
+++ sources/jugfile.py
@@ -1,14 +1,23 @@
+from jug import Task, TaskGenerator
 from leave1out import leave1out
 from projection import random_project
 from randomlearner import random_learner
 from regularized import ridge_regression, lasso_regression
 from preproc import preproc
 import numpy as np
-np.random.seed(322)
 
-features,labels = preproc()
+leave1out = TaskGenerator(leave1out)
+@TaskGenerator
+def print_results(results):
+    with open('results.txt', 'w') as output:
+        for name, val in results.items():
+            print >>output, '%-24s: %s' % (name, val)
 
+preprocessed = Task(preproc)
+features = preprocessed[0]
+labels = preprocessed[1]
 
+results = {}
 for name,learner in [
         ('ridge', ridge_regression()),
         ('lasso(.05)', lasso_regression(.05)),
@@ -18,5 +27,6 @@ for name,learner in [
         ('random', random_learner()),
         ('random_project(12)', random_project(12)),
         ]:
-    val = leave1out(learner, features, labels)
-    print '%-24s: %s' % (name, val)
+    results[name] = leave1out(learner, features, labels)
+
+print_results(results)
