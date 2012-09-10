cd440bb31dbb2a480c0dfc90323f8d9c31d4956a
Luis Pedro Coelho <luis@luispedro.org>
2012-09-10 22:20:44 +0100

    BUG Correctly measure output performance

--- sources/leave1out.py
+++ sources/leave1out.py
@@ -2,10 +2,14 @@ from scipy import stats
 import numpy as np
 
 def _compare(predicted, gold):
+    assert len(predicted) == len(gold)
     active = ~np.isnan(gold)
-    if predicted[active].ptp() == 0:
+    if predicted[active].ptp() == 0 or gold[active].ptp() == 0:
         return (0.,1.)
-    return stats.spearmanr(predicted[active], gold[active])
+    c,p = stats.spearmanr(predicted[active], gold[active])
+    assert not np.isnan(c)
+    assert not np.isnan(p)
+    return c,p
 
 def leave1out(learner, features, labels):
     '''
@@ -20,12 +24,11 @@ def leave1out(learner, features, labels):
         model = learner.train(features[idx], labels[idx])
         predicted.append(model.apply_one(features[i]))
     predicted = np.array(predicted)
-    predicted = predicted.T
 
     corrs = []
     pvals = []
-    for p in predicted:
-        corr,ps = _compare(p, labels.T[i])
+    for p,ell in zip(predicted.T, labels.T):
+        corr,ps = _compare(p, ell)
         corrs.append(corr)
         pvals.append(ps)
     return np.mean(corrs)
