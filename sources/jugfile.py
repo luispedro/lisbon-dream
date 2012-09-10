3e61f254fecd13b46857ae51a63c6b1dbb30088c
Luis Pedro Coelho <luis@luispedro.org>
2012-09-10 14:24:56 +0100

    ENH Multiple forms of loading features

--- sources/jugfile.py
+++ sources/jugfile.py
@@ -13,20 +13,24 @@ def print_results(results):
         for name, val in results.items():
             print >>output, '%-24s: %s' % (name, val)
 
-preprocessed = Task(preproc)
-features = preprocessed[0]
-labels = preprocessed[1]
 
 results = {}
-for name,learner in [
-        ('ridge', ridge_regression()),
-        ('lasso(.05)', lasso_regression(.05)),
-        ('lasso(.5)', lasso_regression(.5)),
-        ('lasso(.8)', lasso_regression(.8)),
-        ('lasso(2)', lasso_regression(2)),
-        ('random', random_learner()),
-        ('random_project(12)', random_project(12)),
-        ]:
-    results[name] = leave1out(learner, features, labels)
+for lname,loader in [
+                ('rna+ge', preproc),
+                ]:
+    preprocessed = Task(preproc)
+    features = preprocessed[0]
+    labels = preprocessed[1]
+
+    for name,learner in [
+            ('ridge', ridge_regression()),
+            ('lasso(.05)', lasso_regression(.05)),
+            ('lasso(.5)', lasso_regression(.5)),
+            ('lasso(.8)', lasso_regression(.8)),
+            ('lasso(2)', lasso_regression(2)),
+            ('random', random_learner()),
+            ('random_project(12)', random_project(12)),
+            ]:
+        results['{0}+{1}'.format(lname,name)] = leave1out(learner, features, labels)
 
 print_results(results)
