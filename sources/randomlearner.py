81b54b2642be4bf571ac58f5465abcbecafc73cf
Luis Pedro Coelho <luis@luispedro.org>
2012-09-13 12:28:22 +0100

    ENH Try multiple random initializations

--- sources/randomlearner.py
+++ sources/randomlearner.py
@@ -1,14 +1,19 @@
 import numpy as np
 class random_result(object):
-    def __init__(self, dim):
+    def __init__(self, dim, R):
         self.dim = dim
+        self.R = R
 
     def apply_one(self, _):
-        return np.random.rand(self.dim)
+        return self.R.rand(self.dim)
 
 class random_learner(object):
     '''
     This does not actually learn anything. It just returns random values
     '''
+    def __init__(self, seed):
+        from milk.utils.utils import get_nprandom
+        self.R = get_nprandom(seed)
+
     def train(self, _, labels):
-        return random_result(len(labels[0]))
+        return random_result(len(labels[0]), self.R)
