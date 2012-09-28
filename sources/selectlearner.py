1c5f1e10a568b5db48819e3647f89cca676cf735
Luis Pedro Coelho <luis@luispedro.org>
2012-09-28 18:40:32 +0100

    ENH Select learner should not be random
    Just pseudo-random

--- sources/selectlearner.py
+++ sources/selectlearner.py
@@ -1,19 +1,21 @@
 from milk.supervised.base import supervised_model
 import numpy as np
-def select1(fi, ells):
+def select1(fi, ells, R):
     C = np.array([np.cov(fi[:,i],ells)[0,1] for i in xrange(len(fi.T))])
-    R = np.array([np.cov(np.random.rand(len(ells)),ells)[0,1] for i in xrange(10000)])
+    R = np.array([np.cov(R.rand(len(ells)),ells)[0,1] for i in xrange(10000)])
     R = np.abs(R)
     R.sort()
     cutoff = R[-len(R)//100]
     return np.abs(C) > cutoff
 
 
-def select(features, labels):
+def select(features, labels, R):
+    from milk.utils.utils import get_nprandom
+    R = get_nprandom(R)
     ss = []
     for ells in labels.T:
         active = ~np.isnan(ells)
-        ss.append(select1(features[active,:], ells[active]))
+        ss.append(select1(features[active,:], ells[active], R))
     ss = np.array(ss)
     return (ss.sum(0) > 20)
 
@@ -25,8 +27,10 @@ class select_model(supervised_model):
         return features[self.mask]
 
 class select_learner(object):
+    def __init__(self, R):
+        self.R = R
     def train(self, features, labels):
-        return select_model(select(features, labels))
+        return select_model(select(features, labels, self.R))
 
 class remove_constant_features(object):
     def train(self, features, labels):
