0a4133391d2fee1998fa4fe39e32ecb887d6045c
Luis Pedro Coelho <luis@luispedro.org>
2012-10-01 14:54:10 +0100

    MIN Brute force variations

--- sources/jugfile.py
+++ sources/jugfile.py
@@ -1,6 +1,7 @@
 from jug import Task, TaskGenerator
 from leave1out import leave1out
-from projection import random_project
+from projection import random_project, random2_project
+from projection import *
 from randomlearner import random_learner
 from selectlearner import *
 from regularized import ridge_regression, lasso_regression, lasso_regression_with_learning
@@ -32,13 +33,21 @@ def prune_features(data, frac):
     return prune_similar(features, frac=frac), labels
 
 @TaskGenerator
-def thresh(features_labels):
-    features, labels = features_labels
+def thresh(data):
+    features = data[0]
+    labels = data[1]
     mu = features.mean(0)
     std = features.std(0)
     features = (features > mu + 2*std) | (features < mu - 2*std)
     return features, labels
 
+
+@TaskGenerator
+def concat_features(data0,data1):
+    features0 = data0[0]
+    features1 = data1[0]
+    labels = data0[1]
+    return np.hstack((features0,features1)), labels
 
 class add_model(supervised_model):
     def __init__(self, base, b):
@@ -95,6 +104,8 @@ ge_rna_valid_mean = Task(ge_rna_valid)
 results = {}
 for lname,data in [
                 ('rna+ge+act+zs', ge_rna_valid_mean),
+                ('thresh(rna+ge+act+zs)', thresh(ge_rna_valid_mean)),
+                ('prune(thresh(rna+ge+act+zs))', prune_features(thresh(ge_rna_valid_mean),.5)),
                 ('prune(rna+ge+act+zs)', prune_features(ge_rna_valid_mean,.5)),
                 ('rna+ge+act(ma)+zs', Task(ge_rna_valid,'maxabs')),
                 ('gow', rna_ge_gosweigths_add),
@@ -110,10 +121,13 @@ for lname,data in [
                 ('prune(gow, .5)', prune_features(rna_ge_gosweigths_add, .5)),
                 ('prune(gowmp, .5)', prune_features(rna_ge_gosweigths_mp_add, .5)),
                 ('thresh(gowmp)', thresh(rna_ge_gosweigths_mp_add)),
+                ('CONCAT thresh(gowmp)+prune(rna+ge+act)', concat_features(thresh(rna_ge_gosweigths_mp_add), prune_features(ge_rna_valid_mean,.5))),
                 ('thresh(gowmpbf)', thresh(rna_ge_gosweigths_mpbf_add)),
                 ('prune(gowmpbf, .5)', prune_features(rna_ge_gosweigths_mpbf_add, .5)),
                 ('prune(thresh(gowmpbf), .5)', prune_features(thresh(rna_ge_gosweigths_mpbf_add), .5)),
+                ('prune(thresh(gowmpbf), .1)', prune_features(thresh(rna_ge_gosweigths_mpbf_add), .1)),
                 ('prune(thresh(gowbf), .5)', prune_features(thresh(rna_ge_gosweigths_bf_add), .5)),
+                ('prune(thresh(gowbf), .1)', prune_features(thresh(rna_ge_gosweigths_bf_add), .1)),
                 ('thresh(gowbf)', thresh(rna_ge_gosweigths_bf_add)),
                 ('prune(gow-ma, .5)', prune_features(rna_ge_gosweigths_maxabs, .5)),
                 ('prune(gow, .9)', prune_features(rna_ge_gosweigths_add, .9)),
@@ -150,14 +164,53 @@ for lname,data in [
             ('zs+sel+min0+lasso_guess', ctransforms(remove_constant_features(), zscore_normalise(), select_learner(12), min_to_zero(), lasso_regression_guess())),
             ('zs+min0+lasso_guess', ctransforms(remove_constant_features(), zscore_normalise(), min_to_zero(), lasso_regression_guess())),
             ('min0+lasso_guess', ctransforms(remove_constant_features(), min_to_zero(), lasso_regression_guess())),
+            ('lasso_guess', ctransforms(remove_constant_features(), lasso_regression_guess())),
             ('min0+lasso_select+ridge', ctransforms(remove_constant_features(), min_to_zero(), lasso_selection(0.000225010113525), ridge_regression(.01))),
 
+            ('rproject(8)', random_project(8)),
             ('rproject(12)', random_project(12)),
+            ('rproject(14)', random_project(14)),
+            ('rproject(16)', random_project(16)),
+            ('rproject(18)', random_project(18)),
             ('rproject(24)', random_project(24)),
-            ('rproject(128, ridge(.01))', random_project(128, ridge_regression(.01))),
-            ('rproject(1024, lasso_learn)', random_project(1024, lasso_regression_with_learning())),
+
+            ('r2project(8)', random2_project(8)),
+            ('r2project(12)', random2_project(12)),
+            ('r2project(14)', random2_project(14)),
+            ('r2project(16)', random2_project(16)),
+            ('r2project(18)', random2_project(18)),
+            ('r2project(24)', random2_project(24)),
+
+            ('rpproject(8)', randomp_project(8)),
+            ('rpproject(12)', randomp_project(12)),
+            ('rpproject(14)', randomp_project(14)),
+            ('rpproject(16)', randomp_project(16)),
+            ('rpproject(18)', randomp_project(18)),
+            ('rpproject(24)', randomp_project(24)),
+
+            ('rp2project(12)', randomp2_project(12)),
+            ('rp2project(24)', randomp2_project(24)),
+
+            ('rs2project(12)', randoms2_project(12)),
+            ('rs2project(24)', randoms2_project(24)),
+            ('rs3project(24)', randoms3_project(24)),
+
+            ('rs2project(64, ridge(.01))', randoms2_project(64, ridge_regression(.01))),
+            ('rs2project(64, lasso_guess)', randoms2_project(64, lasso_regression_guess())),
+            ('rs2project(128, lasso_guess)', randoms2_project(128, lasso_regression_guess())),
+
+
+
+            ('rproject(16, ridge(.01))', random_project(16, ridge_regression(.01))),
+            ('rproject(64, ridge(.01))', random_project(64, ridge_regression(.01))),
+            ('rpproject(64, ridge(.01))', randomp_project(64, ridge_regression(.01))),
+            ('rproject(128, ridge(.01))', randomp_project(128, ridge_regression(.01))),
+            ('rp2roject(128, ridge(.01))', randomp2_project(128, ridge_regression(.01))),
+            ('rproject(256, ridge(.01))', random_project(256, ridge_regression(.01))),
+            #('rproject(1024, lasso_learn)', random_project(1024, lasso_regression_with_learning())),
+            ('rproject(128, lasso_guess)', random_project(128, lasso_regression_guess())),
+            ('rproject(256, lasso_guess)', random_project(256, lasso_regression_guess())),
             ('rproject(1024, lasso_guess)', random_project(1024, lasso_regression_guess())),
-            ('rproject(8192, lasso_guess)', random_project(8192, lasso_regression_guess())),
             ]:
         results[lname,'raw',name] = leave1out(learner, features, labels)
         learner0 = norm_learner(learner, 0)
